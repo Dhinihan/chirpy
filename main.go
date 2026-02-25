b2061ee6e4d458fddc8a9e9318eff1031c2dd17f
Vinícius Nascimento Silva <vinicius.nas.silva@gmail.com>
2026-02-25 20:24:12 -0300

    Adiciona rota de métricas

--- main.go
+++ main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
+	appHandler := http.StripPrefix(
+		"/app",
+		http.FileServer(http.Dir("./app")),
+	)
+
+	cfg := apiConfig{}
+
 	serverMux := http.NewServeMux()
 	serverMux.Handle(
 		"/app/",
-		http.StripPrefix(
-			"/app",
-			http.FileServer(http.Dir("./app")),
-		),
+		cfg.middlewareMetricsInc(appHandler),
 	)
 	serverMux.HandleFunc("/healthz", handleHealthZ)
+	serverMux.HandleFunc("/metrics", cfg.handleMetrics)
+	serverMux.HandleFunc("/reset", cfg.handleReset)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serverMux,
@@ -29,4 +36,36 @@ func handleHealthZ(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, req *http.Request) {
+			cfg.fileserverHits.Add(1)
+			next.ServeHTTP(w, req)
+		})
+
+}
+
+func (cfg *apiConfig) handleMetrics(
+	w http.ResponseWriter,
+	req *http.Request,
+) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handleReset(
+	w http.ResponseWriter,
+	req *http.Request,
+) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits.Load())
 }
