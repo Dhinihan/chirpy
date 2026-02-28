f56f55e27108fad8ddea20e0c49daa345c55a13b
Vinícius Nascimento Silva <vinicius.nas.silva@gmail.com>
2026-02-27 23:15:22 -0300

    Cria endpoint para validar tamanho de mensagem

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"sync/atomic"
 )
@@ -20,6 +22,7 @@ func main() {
 		cfg.middlewareMetricsInc(appHandler),
 	)
 	serverMux.HandleFunc("GET /api/healthz", handleHealthZ)
+	serverMux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
 	serverMux.HandleFunc("GET /admin/metrics", cfg.handleMetrics)
 	serverMux.HandleFunc("POST /admin/reset", cfg.handleReset)
 	server := http.Server{
@@ -36,6 +39,34 @@ func handleHealthZ(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func handleValidateChirp(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong", err)
+		return
+	}
+	var requestData struct {
+		Body string `json:"body"`
+	}
+	if err := json.Unmarshal(data, &requestData); err != nil {
+		respondWithError(w, 400, "expected json with 'body' key", err)
+		return
+	}
+	if requestData.Body == "" {
+		respondWithError(w, 400, "Chirp not informed", nil)
+		return
+	}
+	if len(requestData.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long", nil)
+		return
+	}
+	respondWithJson(w, 200, struct {
+		Valid bool `json:"valid"`
+	}{Valid: true})
+	return
 }
 
 type apiConfig struct {
@@ -72,4 +103,20 @@ func (cfg *apiConfig) handleReset(
 	w.WriteHeader(200)
 	cfg.fileserverHits.Store(0)
 	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits.Load())
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	respondWithJson(w, code, struct {
+		Error string `json:"error"`
+	}{Error: msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	js, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	fmt.Fprintln(w, string(js))
 }
