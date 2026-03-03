1c255ca09867ce77e576f8b39a6d611a7c4b9d00
Vinícius Nascimento Silva <vinicius.nas.silva@gmail.com>
2026-03-03 20:44:11 -0300

    Cria estrutura para testes e2e

---
+++ tests/api_e2e_test.go
@@ -0,0 +1,131 @@
+package tests
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/Dhinihan/chirpy/internal/application/admin"
+	"github.com/Dhinihan/chirpy/internal/application/api"
+	"github.com/Dhinihan/chirpy/internal/database"
+	_ "github.com/lib/pq" // Driver necessário
+	"github.com/pressly/goose/v3"
+	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+// APITestSuite agrupa tudo que precisamos para o teste E2E
+type APITestSuite struct {
+	suite.Suite
+	pgContainer *postgres.PostgresContainer
+	db          *sql.DB
+	ctx         context.Context
+	queries     *database.Queries
+	mux         *http.ServeMux
+}
+
+// Esta função é o ponto de entrada que o 'go test' reconhece
+func TestE2ESuite(t *testing.T) {
+	suite.Run(t, new(APITestSuite))
+}
+
+// SetupSuite roda uma única vez antes de todos os testes da suite
+func (s *APITestSuite) SetupSuite() {
+	s.ctx = context.Background()
+
+	container, err := postgres.Run(s.ctx,
+		"postgres:15-alpine",                 // Imagem desejada
+		postgres.WithDatabase("chirpy_test"), // Nome do banco
+		postgres.WithUsername("user"),        // Usuário
+		postgres.WithPassword("pass"),        // Senha
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(30*time.Second)),
+	)
+	if err != nil {
+		s.T().Fatal("Falha ao iniciar o container:", err)
+	}
+	s.pgContainer = container
+
+	// 2. Pega a URL de conexão (o Testcontainers escolhe uma porta livre no seu PC)
+	connStr, err := container.ConnectionString(s.ctx, "sslmode=disable")
+	if err != nil {
+		s.FailNow("Falha ao obter string de conexão", err)
+	}
+
+	// 3. Abre a conexão real com o banco
+	dbConn, err := sql.Open("postgres", connStr)
+	if err != nil {
+		s.FailNow("Falha ao conectar no banco", err)
+	}
+	s.db = dbConn
+
+	// 4. Inicializa o sqlc com essa conexão
+	s.queries = database.New(s.db)
+
+	// 5. Rodar migrações do Goose
+	err = goose.Up(s.db, "../sql/schema")
+	if err != nil {
+		s.T().Fatal("Falha ao rodar migrações do Goose:", err)
+	}
+
+	s.mux = http.NewServeMux()
+	cfg := admin.NewApiConfig(s.queries)
+	api.RegisterHandlers(cfg, s.mux)
+
+}
+
+// TearDownSuite roda uma única vez após todos os testes terminarem
+func (s *APITestSuite) TearDownSuite() {
+	if s.pgContainer != nil {
+		err := s.pgContainer.Terminate(s.ctx)
+		if err != nil {
+			s.T().Log("Erro ao finalizar o container:", err)
+		}
+	}
+}
+
+func (s *APITestSuite) SetupTest() {
+	err := s.queries.ResetUsers(s.ctx)
+	if err != nil {
+		s.T().Fatalf("Falha ao limpar o banco de dados: %v", err)
+	}
+}
+
+// executeRequest é um helper para não repetir código de boilerpate
+func (s *APITestSuite) executeRequest(method, url, body string) *httptest.ResponseRecorder {
+	req := httptest.NewRequest(method, url, strings.NewReader(body))
+	res := httptest.NewRecorder()
+	s.mux.ServeHTTP(res, req)
+	return res
+}
+
+func (s *APITestSuite) TestPostUsers() {
+	email := "test@example.com"
+	body := fmt.Sprintf(`{"email": "%s"}`, email)
+	res := s.executeRequest("POST", "/api/users", body)
+
+	s.Equal(201, res.Code)
+	s.Contains(res.Body.String(), email)
+	s.Contains(res.Body.String(), `"id"`)
+	s.Contains(res.Body.String(), `"created_at"`)
+	s.Contains(res.Body.String(), `"updated_at"`)
+}
+
+func (s *APITestSuite) TestPostDuplicatedUser() {
+	email := "test@example.com"
+	body := fmt.Sprintf(`{"email": "%s"}`, email)
+	s.executeRequest("POST", "/api/users", body)
+	res := s.executeRequest("POST", "/api/users", body)
+
+	s.Equal(500, res.Code)
+	s.Contains(res.Body.String(), `"Não foi possível criar o usuário"`)
+}
